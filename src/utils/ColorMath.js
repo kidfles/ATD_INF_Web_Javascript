98f7310c3a2b35236d0e774ce296813078389b45
kidfles <166709765+kidfles@users.noreply.github.com>
2026-02-28 16:34:08 +0100

    fix: replaced == with === for strict typing

--- src/utils/ColorMath.js
+++ src/utils/ColorMath.js
@@ -23,11 +23,11 @@ export const ColorMath = {
     hexToHSL(H) {
         // Converteer eerst hex naar RGB
         let r = 0, g = 0, b = 0;
-        if (H.length == 4) {
+        if (H.length === 4) {
             r = "0x" + H[1] + H[1];
             g = "0x" + H[2] + H[2];
             b = "0x" + H[3] + H[3];
-        } else if (H.length == 7) {
+        } else if (H.length === 7) {
             r = "0x" + H[1] + H[2];
             g = "0x" + H[3] + H[4];
             b = "0x" + H[5] + H[6];
@@ -43,7 +43,7 @@ export const ColorMath = {
             s = 0,
             l = 0;
 
-        if (delta == 0)
+        if (delta === 0)
             h = 0;
         else if (cmax == r)
             h = ((g - b) / delta) % 6;
@@ -58,7 +58,7 @@ export const ColorMath = {
             h += 360;
 
         l = (cmax + cmin) / 2;
-        s = delta == 0 ? 0 : delta / (1 - Math.abs(2 * l - 1));
+        s = delta === 0 ? 0 : delta / (1 - Math.abs(2 * l - 1));
         s = +(s * 100).toFixed(1);
         l = +(l * 100).toFixed(1);
 
